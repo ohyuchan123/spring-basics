6a16a1f0b5a9624c958245edf3918ddbbb7d29c6
ohyuchan123 <oyuchan50@gmail.com>
2023-03-07 20:52:16 +0900

    다양한 의존관계 주입 방법

--- src/main/java/basics/core/order/service/OrderServiceImpl.java
+++ src/main/java/basics/core/order/service/OrderServiceImpl.java
@@ -18,17 +18,20 @@ public class OrderServiceImpl implements OrderService{
     private DiscountPolicy discountPolicy;
 //    private final DiscountPolicy discountPolicy;
 
-    @Autowired
+    @Autowired(required = false)
     public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("memberRepository = "+memberRepository);
         this.memberRepository = memberRepository;
     }
     @Autowired
     public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("discountPolicy = "+discountPolicy);
         this.discountPolicy = discountPolicy;
     }
 
     @Autowired //@Autowired 를 사용하면 생성자에서 여러 의존관계도 한번에 주입받을 수 있다.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
